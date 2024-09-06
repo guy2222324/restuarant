fc51034aacb32b1599c77d29c122be9eba098407
Krittipong Nakrachan <krittipong.n@ku.th>
2024-09-06 21:53:22 +0700

    mini_project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { FormsModule } from '@angular/forms';
@@ -14,7 +13,6 @@ import { ReservationComponent } from './reservation/reservation.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent,
     AboutUsComponent,
     ContactUsComponent,
     ReservationComponent
